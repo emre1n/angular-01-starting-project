b126496ba8aac1b3f77cc65b3c1003fa6dbaaaa7
emre1n <emre1n@protonmail.com>
2025-07-23 17:17:59 +0300

    revert back to new for and if syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -9,7 +8,7 @@ import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
